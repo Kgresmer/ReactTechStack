ce3680cd31c54cef7084c9268dab4f98b473a482
kgresmer <kgresmer@gmail.com>
2018-01-03 22:25:45 -0600

    Setup for sending library data as props to a component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
 import {Header} from './components/common'
+import LibraryList from "./components/LibraryList";
 
 export default class App extends Component<{}> {
     render() {
@@ -11,7 +12,7 @@ export default class App extends Component<{}> {
             <Provider store={createStore(reducers)}>
                 <View style={styles.container}>
                     <Header headerText="Redux Learning"/>
-
+                    <LibraryList />
                 </View>
             </Provider>
         );
